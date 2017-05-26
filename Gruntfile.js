cef8b395c70189d416d66338ce17aabbe76534b3
Kan <waterbeside@gmail.com>
2017-05-26 15:31:05 +0800

    build

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+
+      options: {
+        "jshintrc":true      
+      },
+      all: [
+        'src/*.js'
+      ],
+    },
+    uglify: {
+      options: {
+        stripBanner: true,
+        banner: [
+          '/*!',
+          ' <%= pkg.name %>.js v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>)',
+          ' Copyright (c) <%= pkg.author %>',
+          '*/\n'
+        ].join('\n'),
+      },
+      build: {
+        src: 'src/jquery.kanModal.js',
+        dest: 'build/jquery.kanModal.min.js'
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['jshint', 'uglify']);
+}
